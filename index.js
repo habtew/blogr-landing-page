7567c12f2365688de4873b55c33bfcd8ffea0842
Habtamu <habtamuwolde92@gmail.com>
2023-05-01 10:24:22 +0530

    navigation javascript

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const humberger = document.querySelector('.humberger')
 const nav = document.querySelector('.nav')
 
+const product = document.querySelector('.product')
+const productItem = document.querySelector('.nav-item-product')
+
+const connect = document.querySelector('.connect')
+const connectItem = document.querySelector('.nav-item-connect')
+
+const company = document.querySelector('.company')
+const companyItem = document.querySelector('.nav-item-company')
+
 humberger.addEventListener('click', ()=>{
-    nav.classList.toggle('none')
+    nav.classList.toggle('none-nav')
+})
+
+product.addEventListener('click', ()=>{
+    productItem.classList.toggle('none')
+    companyItem.classList.add('none')
+    connectItem.classList.add('none')
+})
+
+company.addEventListener('click', ()=>{
+    companyItem.classList.toggle('none')
+    productItem.classList.add('none')
+    connectItem.classList.add('none')
+    // console.log(productItem)
+})
+
+
+connect.addEventListener('click', ()=>{
+    connectItem.classList.toggle('none')
+    companyItem.classList.add('none')
+    productItem.classList.add('none')
+    // console.log(productItem)
 })
